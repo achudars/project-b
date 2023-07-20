d8889761ac391abb8baa847288abcacaf3eb6fbf
aleks <achudars@gmail.com>
2023-07-20 17:18:08 +0100

    feat: add S-Works Stumpjumper EVO

--- src/Items.tsx
+++ src/Items.tsx
@@ -1,35 +1,51 @@
 import Box from "@mui/material/Box";
 import Masonry from "@mui/lab/Masonry";
-import items from "./data/data.json";
+import bicycles from "./data/data.json";
 import { ImageLabel } from "./ImageLabel.tsx";
 
-export const Items = () => (
-  <Box sx={{ width: 1000, minHeight: 800 }}>
-    <Masonry columns={3} spacing={2}>
-      {items.map((item, index) => (
-        <div
-          key={index}
-          style={{
-            border: "3px solid tomato",
-            background: "FFF",
-          }}
-        >
-          <ImageLabel>{item.title}</ImageLabel>
-          <img
-            src={`./src/images/${item.imgFolder}/${item.mainImg}`}
-            srcSet={`./src/images/${item.imgFolder}/${item.mainImg}`}
-            alt={item.title}
-            loading="lazy"
-            style={{
-              borderBottomLeftRadius: 2,
-              borderBottomRightRadius: 2,
-              display: "block",
+export interface BicycleSpecifications {
+  [key: string]: string;
+}
 
-              width: "100%",
+export interface Bicycle {
+  mainImg: string;
+  imgFolder: string;
+  title: string;
+  subText: string;
+  price: string;
+  url: string;
+  specifications: BicycleSpecifications;
+}
+
+export const Items = () => {
+  return (
+    <Box sx={{ width: 800, minHeight: 800 }}>
+      <Masonry columns={1} spacing={2}>
+        {(bicycles as Bicycle[]).map((item, index) => (
+          <div
+            key={index}
+            style={{
+              border: "3px solid tomato",
+              background: "FFF",
             }}
-          />
-        </div>
-      ))}
-    </Masonry>
-  </Box>
-);
+          >
+            <ImageLabel>{item.title}</ImageLabel>
+            <img
+              src={`./src/images/${item.imgFolder}/${item.mainImg}`}
+              srcSet={`./src/images/${item.imgFolder}/${item.mainImg}`}
+              alt={item.title}
+              loading="lazy"
+              style={{
+                borderBottomLeftRadius: 2,
+                borderBottomRightRadius: 2,
+                display: "block",
+
+                width: "100%",
+              }}
+            />
+          </div>
+        ))}
+      </Masonry>
+    </Box>
+  );
+};
