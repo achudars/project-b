0a9109f41d71091cd69898c8bec3a18e147220db
aleks <achudars@gmail.com>
2023-07-20 15:58:14 +0100

    feat: add Zerode Katipo 29” MTB

--- src/ImageLabel.tsx
+++ src/ImageLabel.tsx
@@ -1,12 +1,14 @@
-import {styled} from "@mui/material/styles";
+import { styled } from "@mui/material/styles";
 import Paper from "@mui/material/Paper";
 
-export const ImageLabel = styled(Paper)(({theme}) => ({
-    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-    ...theme.typography.body2,
-    padding: theme.spacing(0.5),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-    borderBottomLeftRadius: 0,
-    borderBottomRightRadius: 0,
+export const ImageLabel = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(0.5),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+  borderTopLeftRadius: 2,
+  borderTopRightRadius: 2,
+  borderBottomLeftRadius: 0,
+  borderBottomRightRadius: 0,
 }));
